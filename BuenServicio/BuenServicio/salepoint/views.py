c16599f93797611b6d896b092cebd677af7391de
Gaspar Bianco <gasparbianco98@gmail.com>
2023-11-03 18:33:52 -0300

    imprimir comandas

--- BuenServicio/BuenServicio/salepoint/views.py
+++ BuenServicio/BuenServicio/salepoint/views.py
@@ -4,6 +4,8 @@ from .models import Order
 from products.models import Product
 from django.shortcuts import redirect
 from django.contrib import messages
+import win32print
+import win32ui
 
 def home(request):
     tables = Table.objects.all().order_by('number')
@@ -12,13 +14,18 @@ def home(request):
 def table(request, table):
     table = Table.objects.get(number=table)
     order = Order.objects.filter(table=table.id)
+    printers = win32print.EnumPrinters(win32print.PRINTER_ENUM_LOCAL, None, 1)
+    printers_names = []
+    for i in range(len(printers)):
+        printers_names.append(printers[i][2])
     total = 0
     for i in order:
         total += i.total
     context = { 'table':table,
                 'order': order,
                 'total': total,
-                'message': list(messages.get_messages(request))}                
+                'message': list(messages.get_messages(request)),
+                'printers': printers_names}                
     
     return render(request, 'salepoint/table.html', context)
 
@@ -28,12 +35,33 @@ def order(request, table_number):
     id_list = request.POST.getlist('id')
     table_id = Table.objects.get(id = request.POST.get('table-id'))
     Table.objects.filter(id = table_id.id).update(available=False)
+    hprinter = win32ui.CreateDC()
+    hprinter.CreatePrinterDC(request.POST.get('printer'))
+    hprinter.StartDoc('Comanda')
+    hprinter.StartPage()
+    command = f"Mesa {table_number}"
+    hprinter.TextOut(100,100,command)
+    command = request.POST.get('comment')
+    hprinter.TextOut(100,200,command)
+    command = "Producto                    Cantidad"
+    hprinter.TextOut(100,300,command)
     for i in range(len(request.POST.getlist('id'))):
         product = Product.objects.get(id=id_list[i])
         quantity = quantity_list[i]
         total = product.cost * int(quantity)
         if int(quantity) != 0:
             Order(product=product, quantity=int(quantity), table=table_id, total = total).save()
+            
+            
+            command = f"{product.name}"
+            hprinter.TextOut(100,300+100*(i+1),command)
+            command = f"{quantity}"
+            hprinter.TextOut(700,300+100*(i+1),command)
+
+    hprinter.EndPage()
+    hprinter.EndDoc()
+
+
     return redirect('table', table=table_number)
 
 def reset_table(request, table_number):
